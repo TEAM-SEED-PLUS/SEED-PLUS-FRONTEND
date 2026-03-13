a5d55141710d8a354c586cc87281ad26fc3e6c69
JunSeoChoi <evan7484@gmail.com>
2026-03-13 15:48:22 +0900

    [DESING]: 헤더바 컴포넌트 생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Router from '@/routes';
+import Router from '@/routes/router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
