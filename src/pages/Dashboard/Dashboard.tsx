38711a02caedc62317e950ce904de69f5519fdc1
JunSeoChoi <evan7484@gmail.com>
2026-03-13 16:54:11 +0900

    [DESIGN]: MLS 헤더바 컴포넌트 생성

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/layout/Header';
 
 const Dashboard = () => {
   return (
-    <div className="w-full min-h-screen">
+    <div className="w-full min-h-screen bg-gray-100">
       <Header />
     </div>
   );
