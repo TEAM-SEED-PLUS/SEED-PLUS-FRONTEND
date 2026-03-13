354d2ae292fe5bd11a0031589d370387911acfe8
JunSeoChoi <evan7484@gmail.com>
2026-03-13 16:54:43 +0900

    [DESIGN]: MLS 헤더바 컴포넌트 생성

---
+++ src/pages/MLS/MlsPage.tsx
@@ -0,0 +1,11 @@
+import HeaderMLS from '@/components/layout/HeaderMLS';
+
+const MlsPage = () => {
+  return (
+    <div className="w-full min-h-screen bg-gray-500">
+      <HeaderMLS />
+    </div>
+  );
+};
+
+export default MlsPage;
