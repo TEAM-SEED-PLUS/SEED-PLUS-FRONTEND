2ef80a6e046dbcb09fbf3229a43242374a8116bb
JunSeoChoi <evan7484@gmail.com>
2026-05-30 03:01:26 +0900

    [DESIGN]: 내 상가 만들기 사이드바 제휴 파트너 영역 변경

--- src/components/layout/HeaderUser.tsx
+++ src/components/layout/HeaderUser.tsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
-import SEEDPLUS from '@/assets/SEED+ LOGO.svg';
+import SEEDPLUS from '@/assets/Logo/SEED+ LOGO.svg';
 import { useAuth } from '@/auth';
 
 type UserNav = 'home' | 'feed' | 'store';
