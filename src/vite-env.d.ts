8e78acd2c1ea178ec7eb262b29a3353a004c1b5e
JunSeoChoi <evan7484@gmail.com>
2026-05-30 18:09:03 +0900

    [UPDATE]: API URL 환경별 분기 처리 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL?: string;
+  readonly VITE_APP_ENV?: 'development' | 'production';
+  readonly VITE_DEV_API_BASE_URL?: string;
+  readonly VITE_PROD_API_BASE_URL?: string;
 }
 
 interface ImportMeta {
